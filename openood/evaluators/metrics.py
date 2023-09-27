e5871e3f208d0afb829f675d56a3cbf997c6d0d0
longbai <100130932+longbai1006@users.noreply.github.com>
2023-09-28 01:59:06 +0800

    Add files via upload

---
+++ openood/evaluators/metrics.py
@@ -0,0 +1,293 @@
+import numpy as np
+from sklearn import metrics
+
+
+def compute_all_metrics(conf, label, pred):
+    np.set_printoptions(precision=3)
+    recall = 0.95
+    fpr, thresh = fpr_recall(conf, label, recall)
+    auroc, aupr_in, aupr_out = auc(conf, label)
+
+    ccr_1 = ccr_fpr(conf, 0.1, pred, label)
+    ccr_2 = ccr_fpr(conf, 0.01, pred, label)
+    ccr_3 = ccr_fpr(conf, 0.001, pred, label)
+    ccr_4 = ccr_fpr(conf, 0.0001, pred, label)
+
+    accuracy = acc(pred, label)
+
+    results1 = np.array(
+        [fpr, auroc, aupr_in, aupr_out, ccr_4, ccr_3, ccr_2, ccr_1, accuracy])
+
+    results = [
+        fpr, auroc, aupr_in, aupr_out, ccr_4, ccr_3, ccr_2, ccr_1, accuracy
+    ]
+
+    return results
+
+
+# accuracy
+def acc(pred, label):
+    ind_pred = pred[label != -1]
+    ind_label = label[label != -1]
+
+    num_tp = np.sum(ind_pred == ind_label)
+    acc = num_tp / len(ind_label)
+
+    return acc
+
+
+# fpr_recall
+def fpr_recall(conf, label, tpr):
+    # ind_conf = conf[label != -1]
+    # ood_conf = conf[label == -1]
+    # num_ind = len(ind_conf)
+    # num_ood = len(ood_conf)
+    gt = np.ones_like(label)
+    gt[label == -1] = 0
+    # recall_num = int(np.floor(tpr * num_ind))
+    # thresh = np.sort(ind_conf)[-recall_num]
+    # num_fp = np.sum(ood_conf > thresh)
+    # fpr = num_fp / num_ood
+
+    fpr_list, tpr_list, threshold_list = metrics.roc_curve(gt, conf)
+    fpr = fpr_list[np.argmax(tpr_list >= tpr)]
+    thresh = threshold_list[np.argmax(tpr_list >= tpr)]
+    return fpr, thresh
+
+
+# auc
+def auc(conf, label):
+
+    ind_indicator = np.zeros_like(label)
+    ind_indicator[label != -1] = 1
+
+    fpr, tpr, thresholds = metrics.roc_curve(ind_indicator, conf)
+
+    precision_in, recall_in, thresholds_in \
+        = metrics.precision_recall_curve(ind_indicator, conf)
+
+    precision_out, recall_out, thresholds_out \
+        = metrics.precision_recall_curve(1 - ind_indicator, 1 - conf)
+
+    auroc = metrics.auc(fpr, tpr)
+    aupr_in = metrics.auc(recall_in, precision_in)
+    aupr_out = metrics.auc(recall_out, precision_out)
+
+    return auroc, aupr_in, aupr_out
+
+
+# ccr_fpr
+def ccr_fpr(conf, fpr, pred, label):
+    ind_conf = conf[label != -1]
+    ind_pred = pred[label != -1]
+    ind_label = label[label != -1]
+
+    ood_conf = conf[label == -1]
+
+    num_ind = len(ind_conf)
+    num_ood = len(ood_conf)
+
+    fp_num = int(np.ceil(fpr * num_ood))
+    thresh = np.sort(ood_conf)[-fp_num]
+    num_tp = np.sum((ind_conf > thresh) * (ind_pred == ind_label))
+    ccr = num_tp / num_ind
+
+    return ccr
+
+
+def detection(ind_confidences,
+              ood_confidences,
+              n_iter=100000,
+              return_data=False):
+    # calculate the minimum detection error
+    Y1 = ood_confidences
+    X1 = ind_confidences
+
+    start = np.min([np.min(X1), np.min(Y1)])
+    end = np.max([np.max(X1), np.max(Y1)])
+    gap = (end - start) / n_iter
+
+    best_error = 1.0
+    best_delta = None
+    all_thresholds = []
+    all_errors = []
+    for delta in np.arange(start, end, gap):
+        tpr = np.sum(np.sum(X1 < delta)) / np.float(len(X1))
+        error2 = np.sum(np.sum(Y1 > delta)) / np.float(len(Y1))
+        detection_error = (tpr + error2) / 2.0
+
+        if return_data:
+            all_thresholds.append(delta)
+            all_errors.append(detection_error)
+
+        if detection_error < best_error:
+            best_error = np.minimum(best_error, detection_error)
+            best_delta = delta
+
+    if return_data:
+        return best_error, best_delta, all_errors, all_thresholds
+    else:
+        return best_error, best_delta
+
+
+import os
+import sys
+import numpy as np
+
+def get_curve_online(known, novel, stypes = ['Bas']):
+    tp, fp = dict(), dict()
+    tnr_at_tpr95 = dict()
+    for stype in stypes:
+        known.sort()
+        novel.sort()
+        end = np.max([np.max(known), np.max(novel)])
+        start = np.min([np.min(known),np.min(novel)])
+        num_k = known.shape[0]
+        num_n = novel.shape[0]
+        tp[stype] = -np.ones([num_k+num_n+1], dtype=int)
+        fp[stype] = -np.ones([num_k+num_n+1], dtype=int)
+        tp[stype][0], fp[stype][0] = num_k, num_n
+        k, n = 0, 0
+        for l in range(num_k+num_n):
+            if k == num_k:
+                tp[stype][l+1:] = tp[stype][l]
+                fp[stype][l+1:] = np.arange(fp[stype][l]-1, -1, -1)
+                break
+            elif n == num_n:
+                tp[stype][l+1:] = np.arange(tp[stype][l]-1, -1, -1)
+                fp[stype][l+1:] = fp[stype][l]
+                break
+            else:
+                if novel[n] < known[k]:
+                    n += 1
+                    tp[stype][l+1] = tp[stype][l]
+                    fp[stype][l+1] = fp[stype][l] - 1
+                else:
+                    k += 1
+                    tp[stype][l+1] = tp[stype][l] - 1
+                    fp[stype][l+1] = fp[stype][l]
+        tpr95_pos = np.abs(tp[stype] / num_k - .95).argmin()
+        tnr_at_tpr95[stype] = 1. - fp[stype][tpr95_pos] / num_n
+    return tp, fp, tnr_at_tpr95
+
+def metric_ood(x1, x2, stypes = ['Bas'], verbose=False):
+    tp, fp, tnr_at_tpr95 = get_curve_online(x1, x2, stypes)
+    results = dict()
+    mtypes = ['TNR', 'AUROC', 'DTACC', 'AUIN', 'AUOUT']
+    if verbose:
+        print('      ', end='')
+        for mtype in mtypes:
+            print(' {mtype:6s}'.format(mtype=mtype), end='')
+        print('')
+        
+    for stype in stypes:
+        if verbose:
+            print('{stype:5s} '.format(stype=stype), end='')
+        results[stype] = dict()
+        
+        # TNR
+        mtype = 'TNR'
+        results[stype][mtype] = 100.*tnr_at_tpr95[stype]
+        if verbose:
+            print(' {val:6.3f}'.format(val=results[stype][mtype]), end='')
+        
+        # AUROC
+        mtype = 'AUROC'
+        tpr = np.concatenate([[1.], tp[stype]/tp[stype][0], [0.]])
+        fpr = np.concatenate([[1.], fp[stype]/fp[stype][0], [0.]])
+        results[stype][mtype] = 100.*(-np.trapz(1.-fpr, tpr))
+        if verbose:
+            print(' {val:6.3f}'.format(val=results[stype][mtype]), end='')
+        
+        # DTACC
+        mtype = 'DTACC'
+        results[stype][mtype] = 100.*(.5 * (tp[stype]/tp[stype][0] + 1.-fp[stype]/fp[stype][0]).max())
+        if verbose:
+            print(' {val:6.3f}'.format(val=results[stype][mtype]), end='')
+        
+        # AUIN
+        mtype = 'AUIN'
+        denom = tp[stype]+fp[stype]
+        denom[denom == 0.] = -1.
+        pin_ind = np.concatenate([[True], denom > 0., [True]])
+        pin = np.concatenate([[.5], tp[stype]/denom, [0.]])
+        results[stype][mtype] = 100.*(-np.trapz(pin[pin_ind], tpr[pin_ind]))
+        if verbose:
+            print(' {val:6.3f}'.format(val=results[stype][mtype]), end='')
+        
+        # AUOUT
+        mtype = 'AUOUT'
+        denom = tp[stype][0]-tp[stype]+fp[stype][0]-fp[stype]
+        denom[denom == 0.] = -1.
+        pout_ind = np.concatenate([[True], denom > 0., [True]])
+        pout = np.concatenate([[0.], (fp[stype][0]-fp[stype])/denom, [.5]])
+        results[stype][mtype] = 100.*(np.trapz(pout[pout_ind], 1.-fpr[pout_ind]))
+        if verbose:
+            print(' {val:6.3f}'.format(val=results[stype][mtype]), end='')
+            print('')
+    
+    return results
+
+def compute_oscr(pred_k, pred_u, labels):
+    x1, x2 = np.max(pred_k, axis=1), np.max(pred_u, axis=1)
+    pred = np.argmax(pred_k, axis=1)
+    correct = (pred == labels)
+    m_x1 = np.zeros(len(x1))
+    m_x1[pred == labels] = 1
+    k_target = np.concatenate((m_x1, np.zeros(len(x2))), axis=0)
+    u_target = np.concatenate((np.zeros(len(x1)), np.ones(len(x2))), axis=0)
+    predict = np.concatenate((x1, x2), axis=0)
+    n = len(predict)
+
+    # Cutoffs are of prediction values
+    
+    CCR = [0 for x in range(n+2)]
+    FPR = [0 for x in range(n+2)] 
+
+    idx = predict.argsort()
+
+    s_k_target = k_target[idx]
+    s_u_target = u_target[idx]
+
+    for k in range(n-1):
+        CC = s_k_target[k+1:].sum()
+        FP = s_u_target[k:].sum()
+
+        # True	Positive Rate
+        CCR[k] = float(CC) / float(len(x1))
+        # False Positive Rate
+        FPR[k] = float(FP) / float(len(x2))
+
+    CCR[n] = 0.0
+    FPR[n] = 0.0
+    CCR[n+1] = 1.0
+    FPR[n+1] = 1.0
+
+    # Positions of ROC curve (FPR, TPR)
+    ROC = sorted(zip(FPR, CCR), reverse=True)
+
+    OSCR = 0
+
+    # Compute AUROC Using Trapezoidal Rule
+    for j in range(n+1):
+        h =   ROC[j][0] - ROC[j+1][0]
+        w =  (ROC[j][1] + ROC[j+1][1]) / 2.0
+
+        OSCR = OSCR + h*w
+
+    return OSCR
+
+
+def F_measure(preds, labels, theta=None):
+    # f1 score for openset evaluation
+    true_pos = 0.
+    false_pos = 0.
+    false_neg = 0.
+    for i in range(len(labels)):
+        true_pos += 1 if preds[i] == labels[i] and labels[i] != -1 else 0
+        false_pos += 1 if preds[i] != labels[i] and labels[i] != -1 else 0
+        false_neg += 1 if preds[i] != labels[i] and labels[i] == -1 else 0
+
+    precision = true_pos / (true_pos + false_pos) 
+    recall = true_pos / (true_pos + false_neg + 1e-12)
+    return 2 * ((precision * recall) / (precision + recall + 1e-12))
